558f2a2abbc7587ff56d0b24eb60cf030e36b297
Tom Lever <46533664+tslever@users.noreply.github.com>
2025-06-03 18:20:47 -0400

    Create bucket_sort.py

---
+++ Module_2/bucket_sort.py
@@ -0,0 +1,15 @@
+'''
+Bucket Sort
+
+
+Time complexity is O(n + k), where k is the maximum number in the list.
+Finding the maximum number requires n reads.
+The new list has k elements.
+
+
+• Suppose we need to sort an array of positive integers {3, 11, 2, 9, 1, 7} (input array)
+• Procedure:
+    • Create an array of size 11 (largest number)
+    • Go through the input array and place 3 into a second array at index 3, integer 11 at index 11 and so on
+    • Result: sorted list in the second array
+'''
