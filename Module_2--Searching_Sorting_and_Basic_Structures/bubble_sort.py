d03a6de78a03fb2e4cfd3d6636afbdc25eae9fe0
Tom Lever <46533664+tslever@users.noreply.github.com>
2025-07-02 19:03:58 -0400

    Update bubble_sort.py

--- Module_2--Searching_Sorting_and_Basic_Structures/bubble_sort.py
+++ Module_2--Searching_Sorting_and_Basic_Structures/bubble_sort.py
@@ -18,7 +18,21 @@ def bubble_sort(data):
     '''
     # For each element at index i from 0 inclusive to n exclusive...
     for i in range(0, len(data)):
-        # For each element at index j from n - 1 inclusive to i exclusive...
+        '''
+        A pass is 1 complete execution of the loop with index i.
+
+        j starts as the greatest / right-most index of the unsorted part of the array / part starting at i and decreases / moves left.
+        When the element at j is smaller than the element at j - 1 / the element to the left,
+        these elements are swapped.
+        A smaller element is pushed / bubbled left.
+        After the inner loop finishes / 1 pass, the smallest element in unsorted part of the array / part starting at i
+        has bubbled left to position i.
+        i / the left edge of the unsorted part of the array is increased / moved right.
+
+        Bubble sort could be written with j increasing and swapping occurring when data[j] > data[j + 1].
+        In each pass, adjacent elements are compared and the "out of order" element is moved
+        to its appropriate end of the unosorted region.
+        '''
         for j in range(len(data) - 1, i, -1):
             # If the element at j is less than that at j - 1, swap them.
             if data[j] < data[j - 1]:
@@ -35,7 +49,8 @@ def swap(data, x, y):
 
 if __name__ == "__main__":
     
-    data = [7, 5, 2, 4, 3, 9]
+    #data = [7, 5, 2, 4, 3, 9]
+    data = [5, 1, 4, 2, 8]
 
     print("Data is [" + ", ".join(str(datum) for datum in data) + "].\n")
     bubble_sort(data)
