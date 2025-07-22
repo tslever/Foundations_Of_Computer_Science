4cad230faeb5b89ca909325f103e688f2386baf4
Tom Lever <46533664+tslever@users.noreply.github.com>
2025-07-22 11:07:47 -0400

    Correct net flow and generalize from ICA.

--- Module_8--Classic_CS/ICA.py
+++ Module_8--Classic_CS/ICA.py
@@ -1,3 +1,43 @@
+'''
+In class group assignment:
+Consider the flow network below with source s, sink t and above capacities.
+For example, the capacity between the source and node 1 is 16.
+
+Write the flow on each edge to maximize flow over this network.
+The dictionary of edges, flows, and capacities corresponding to the maximum flow from s to t / over the graph is
+{('s', '1'): '12/16', ('s', '2'): '11/12', ('1', '3'): '12/12', ('2', '1'): '0/4', ('2', '4'): '11/14', ('3', '2'): '0/9', ('3', 't'): '19/20', ('4', '3'): '7/7', ('4', 't'): '4/4'}
+
+What is the maximum flow?
+The maximum flow from s to t / over the graph is 23.
+
+For all nodes that are not source or sink, what can be said about their net flow?
+For every node v that is not source or sink, the flow-conservation constraint applies.
+The sum along all edges ending with v of all inflows to v is equal to the sum along all edges starting with v of all outflows from v.
+The net flow of v is the difference between the inflow to v and the outflow from v.
+Since the inflow to v and outflow from v are equal, the net flow of v is 0.
+
+What can be said about the net flow for the source?
+The net flow of the source s is equal to the difference between the inflow to s and the outflow from s.
+The inflow to s is 0.
+The net flow of s is the negative outflow from s.
+All flow leaves s.
+The source supplies all flow to the graph.
+The maximum flow is 23.
+The outflow from s is 23.
+The net flow of s is -23.
+
+What can be said about the net flow for the sink?
+The net flow of the sink t is equal to the difference between the inflow to t and the outflow from t.
+The outflow from t is 0.
+The net flow of t is the inflow to t.
+All flow comes to t.
+The sink receives all flow from the graph.
+The maximum flow is 23.
+The inflow to t is 23.
+The net flow of t is 23.
+'''
+
+
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -46,9 +86,11 @@ dictionary_of_edges_flows_and_capacities = {
 }
 
 print(
+    "In class group assignment:\n" +
     "Consider the flow network below with source s, sink t and above capacities.\n" +
     "For example, the capacity between the source and node 1 is 16.\n\n" +
     
+    "Write the flow on each edge to maximize flow over this network.\n" +
     "The dictionary of edges, flows, and capacities corresponding to the maximum flow from s to t / over the graph is\n" +
     f"{dictionary_of_edges_flows_and_capacities}\n\n" +
 
@@ -59,29 +101,28 @@ print(
     "For every node v that is not source or sink, the flow-conservation constraint applies.\n" +
     "The sum along all edges ending with v of all inflows to v is equal to " +
     "the sum along all edges starting with v of all outflows from v.\n" +
-    "The net flow of v is the difference between the outflow from v and the inflow to v.\n" +
+    "The net flow of v is the difference between the inflow to v and the outflow from v.\n" +
     "Since the inflow to v and outflow from v are equal, the net flow of v is 0.\n\n" +
 
     "What can be said about the net flow for the source?\n" +
-    "The net flow of the source s is equal to the difference between the outflow from s and the inflow to s.\n" +
+    "The net flow of the source s is equal to the difference between the inflow to s and the outflow from s.\n" +
     "The inflow to s is 0.\n" +
-    "The net flow of s is the outflow from s.\n" +
+    "The net flow of s is the negative outflow from s.\n" +
     "All flow leaves s.\n" +
     "The source supplies all flow to the graph.\n" +
     "The maximum flow is 23.\n" +
     "The outflow from s is 23.\n" +
-    "The net flow of s is 23.\n\n" +
+    "The net flow of s is -23.\n\n" +
 
     "What can be said about the net flow for the sink?\n"
-    "The net flow of the sink t is equal to the difference between the outflow from t and the inflow to t.\n" +
+    "The net flow of the sink t is equal to the difference between the inflow to t and the outflow from t.\n" +
     "The outflow from t is 0.\n" +
-    "The net flow of t is the negative inflow to t.\n" +
     "The net flow of t is the inflow to t.\n" +
     "All flow comes to t.\n" +
     "The sink receives all flow from the graph.\n" +
     "The maximum flow is 23.\n" +
     "The inflow to t is 23.\n" +
-    "The net flow of t is -23."
+    "The net flow of t is 23."
 )
 
 nx.draw_networkx_nodes(graph, dictionary_of_nodes_and_positions)
